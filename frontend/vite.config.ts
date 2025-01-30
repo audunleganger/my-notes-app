1f16cc96ca629d2155d078a17157f2205498dd10
Audun Leganger <audun.leganger@gmail.com>
2025-01-30 05:44:45 +0000

    Start backend implementation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-// expose host 3000, accept only connections from host system
 export default defineConfig({
     plugins: [react()],
     server: {
